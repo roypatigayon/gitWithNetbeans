75f0602ecdfc78276bb752f671f5bbf950002ae1
Roy Nino Patigayon <roynino.patigayon@cit.edu>
2023-10-01 19:45:59 +0800

    initial code

---
+++ src/hellotogit/HelloToGit.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package hellotogit;
+
+/**
+ *
+ * @author Roy Patigayon
+ */
+public class HelloToGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hello to git");
+    }
+    
+}
