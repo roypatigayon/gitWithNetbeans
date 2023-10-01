6b54f901fb34217bd7ba4b4bf96934e8a0dee437
Roy Nino Patigayon <roynino.patigayon@cit.edu>
2023-10-01 20:09:34 +0800

    add function for message

--- src/hellotogit/HelloToGit.java
+++ src/hellotogit/HelloToGit.java
@@ -14,7 +14,10 @@ public class HelloToGit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("hello to git");
+        showMessage();
     }
     
+    public static void showMessage(){
+        System.out.println("hello to git");
+    }
 }
